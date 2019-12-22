a8345d75adb0452df0b565bead23df2bd0cae7a6
Anna Malik <anka98@op.pl>
2019-12-22 21:09:58 +0100

    init

---
+++ bd-projekt/src/test/java/projekt/bdprojekt/BdProjektApplicationTests.java
@@ -0,0 +1,13 @@
+package projekt.bdprojekt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BdProjektApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
