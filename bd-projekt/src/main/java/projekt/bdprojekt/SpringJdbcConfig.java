eb90bd124ef1b0f735fbb5cd818cd847c9b214b3
Anna Malik <anka98@op.pl>
2019-12-22 21:25:03 +0100

    conf

---
+++ bd-projekt/src/main/java/projekt/bdprojekt/SpringJdbcConfig.java
@@ -0,0 +1,18 @@
+
+
+
+
+@Configuration
+@ComponentScan("com.baeldung.jdbc")
+public class SpringJdbcConfig {
+    @Bean
+    public DataSource mysqlDataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        dataSource.setUrl("postgres://xczhpocz:agpWVGhTJDWVDjDINL4z8ZzsxU4nWPZl@rajje.db.elephantsql.com:5432/xczhpocz");
+        dataSource.setUsername("xczhpocz");
+        dataSource.setPassword("agpWVGhTJDWVDjDINL4z8ZzsxU4nWPZl");
+ 
+        return dataSource;
+    }
+}
