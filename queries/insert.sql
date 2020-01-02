fc6d2a290df0896f1061c56ffc251f2a245ac5ed
amalik <anna.malik@comarch.pl>
2020-01-02 10:16:43 +0100

    inserts

---
+++ queries/insert.sql
@@ -0,0 +1,12 @@
+insert into pracownik (id_pracownik, imie, nazwisko, pesel, obywatelstwo, stanowisko) values 
+		      (1, 'Augustyn', 'Skalski', ...., 'PL', 'pilot'),
+		      (2, 'Jenny', 'Watson', ...., 'GB', 'steward'),
+		      (3, 'Augustyn', 'Skalski', ...., 'PL', 'stewardessa');
+
+insert into lot (id_lot, lotnisko_wylot, lotnisko_przylot, wylot, przylot) values 
+		      (1, 'WMI', 'CRL', '2020-06-22 19:10:25', '2020-06-22 21:20:25'),
+		      (2, 'WMI', 'LBA', '2020-03-18 13:20:25', '2020-03-18 15:00:25'),
+		      (3, 'CRL', 'WMI', '2020-06-22 21:40:25', '2020-06-22 23:50:25'),
+		      (4, 'LUZ', 'ANR', '2019-12-23 14:00:25', '2019-12-23 16:35:25'),
+		      (5, 'WMI', 'RMI', '2020-03-30 20:55:25', '2020-03-30 22:55:25');
+
