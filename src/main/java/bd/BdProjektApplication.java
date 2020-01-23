ee7c60327a6d59a06510a9688a23ef26bbf175f4
Anna Malik <anka98@op.pl>
2020-01-23 09:20:37 +0100

    test

--- src/main/java/bd/BdProjektApplication.java
+++ src/main/java/bd/BdProjektApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BdProjektApplication {
+	//test
 
 	public static void main(String[] args) {
 		SpringApplication.run(BdProjektApplication.class, args);
