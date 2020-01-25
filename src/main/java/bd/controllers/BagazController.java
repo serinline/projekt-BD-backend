29bd3aeaebc7a7b674bf9f994056d0dd27ab98f3
Anna Malik <anka98@op.pl>
2020-01-25 23:42:59 +0100

    added path

--- src/main/java/bd/controllers/BagazController.java
+++ src/main/java/bd/controllers/BagazController.java
@@ -30,6 +30,6 @@ public class BagazController {
         bagazRepository.usun(bagaz_id);
     }
 
-    @GetMapping
+    @GetMapping("/bagaze")
     void wyswietlBagaze() { bagazRepository.wyswietl();}
 }
