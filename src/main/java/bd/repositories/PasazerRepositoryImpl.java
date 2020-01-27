10372fbac9682b7a7a82aaaca7d6608f82eedc1e
amalik <anna.malik@comarch.pl>
2020-01-27 14:02:52 +0100

    changed impl

--- src/main/java/bd/repositories/PasazerRepositoryImpl.java
+++ src/main/java/bd/repositories/PasazerRepositoryImpl.java
@@ -52,7 +52,8 @@ public class PasazerRepositoryImpl implements PasazerRepository {
                 pasazer.getObywatelstwo(),
                 keyHolder);
 
-        return keyHolder.getKey().intValue();
+        Number key = keyHolder.getKey();
+        return key.intValue();
     }
 
     @Override
